5aa29746c1893c084bffce710450ecd1946f7974
zone <zonetw.web@gmail.com>
2019-02-07 00:59:49 +0800

    add then method and test

--- __tests__/Promise.ts
+++ __tests__/Promise.ts
@@ -8,3 +8,42 @@ describe("1. Basic Test", ()=>{
         expect(promise.status).toEqual(PromiseStatus.PENDING);
     });
 });
+
+describe("The Then Method", ()=>{
+    // involved inner process, hard to check from outside
+    // it("onFulfilled has to be called after promise resolved", ()=>{});
+
+    it("then must return a promise", ()=>{
+        const promise1 = new Promise();
+        const promise2 = promise1.then((result)=>{});
+        // redundant check: discovered by ts
+        // expect(promise2 instanceof Promise).toBe(true);
+        expect(promise1 === promise2).toBe(false);
+    });
+
+    it("then must return a new promise", ()=>{
+        const promise1 = new Promise((resolve, reject)=>{
+            resolve(1);
+        });
+        const promise2 = promise1.then((result)=>{
+            expect(result).toBe(1);
+        });
+        expect(promise1 === promise2).toBe(false);
+    });
+
+    it("If onFulfilled throws an exception e, next promise must be rejected with e as the reason.", ()=>{
+        let tmpReason;
+        const promise1 = new Promise((resolve, reject)=>{
+            resolve();
+        }).then((result)=>{
+            throw new Error("GG");
+        }).then(undefined, (reason)=>{
+            tmpReason = reason;
+        });
+
+        // because try catch, the assert of jest will be blocked, so I check result at next tick
+        process.nextTick(()=>{
+            expect(tmpReason.toString()).toBe("Error: GG");
+        })
+    });
+});
--- src/Promise.ts
+++ src/Promise.ts
@@ -1,12 +1,98 @@
 import {PromiseStatus} from "./PromiseStatus";
 
+type FulfilledAction = (result?)=>any;
+type FailedAction = (reason?)=>any;
+
 export class Promise{
+    get value(): any {
+        return this._value;
+    }
     get status(): PromiseStatus {
         return this._status;
     }
     private _status: PromiseStatus;
+    private _value: any;
+    private _onFulfilledActions: FulfilledAction[];
+    private _onFailedActions: FailedAction[];
 
-    constructor(executor: (resolve:(result)=>void, reject:(reason)=>void)=>void){
+    constructor(executor?: (resolve:(result?)=>void, reject:(reason?)=>void)=>void){
         this._status = PromiseStatus.PENDING;
+        this._value = undefined;
+        this._onFulfilledActions = [];
+        this._onFailedActions = [];
+        this._resolve = this._resolve.bind(this);
+        this._reject = this._reject.bind(this);
+
+        if(executor){
+            executor(this._resolve, this._reject);
+        }
+    }
+
+    private _resolve(result?: any){
+        if(this._status === PromiseStatus.PENDING){
+            this._status = PromiseStatus.RESOLVED;
+            this._value = result;
+
+            this._onFulfilledActions.forEach((action: Function)=>{
+                action(this._value);
+            });
+        }
+    }
+
+    private _reject(reason?: any){
+        if(this._status === PromiseStatus.PENDING){
+            this._status = PromiseStatus.REJECTED;
+            this._value = reason;
+
+            this._onFulfilledActions.forEach((action: Function)=>{
+                action(this._value);
+            });
+        }
+    }
+
+    // A promise’s then method accepts two arguments: onFulfilled and onRejected
+    // Both onFulfilled and onRejected are optional arguments:
+    then(onFulfilled?: FulfilledAction, onRejected?:FailedAction): Promise{
+        const promise = new Promise();
+
+        if(onFulfilled){
+            const wrappedAction = (result)=>{
+                try{
+                    const output = onFulfilled(result);
+                    promise._resolve(output);
+                }catch(e){
+                    promise._reject(e);
+                }
+            };
+            switch(this.status){
+                case PromiseStatus.PENDING:
+                    this._onFulfilledActions.push(wrappedAction);
+                    break;
+                case PromiseStatus.RESOLVED:
+                    wrappedAction(this.value);
+                    break;
+            }
+        }
+
+        if(onRejected){
+            const wrappedAction = (reason)=>{
+                try{
+                    const output =  onRejected(reason);
+                    promise._reject(output);
+                }catch(e){
+                    promise._reject(e);
+                }
+            };
+            switch(this._status){
+                case PromiseStatus.PENDING:
+                    this._onFailedActions.push(wrappedAction);
+                    break;
+                case PromiseStatus.REJECTED:
+                    wrappedAction(this.value);
+                    break;
+            }
+        }
+
+        return promise;
     }
 }
