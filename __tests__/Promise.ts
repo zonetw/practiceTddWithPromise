1b378979a795889eeb8d74d7cc95c6ed776dc3cf
zone <zonetw.web@gmail.com>
2019-02-07 08:55:27 +0800

    add basic tests

--- __tests__/Promise.ts
+++ __tests__/Promise.ts
@@ -8,62 +8,54 @@ describe("1. Basic Test", ()=>{
         expect(promise.status).toEqual(PromiseStatus.PENDING);
     });
 
-    // it("Catch the error occur when executing the executor function ( sync )", ()=>{
-    //     let promise = new Promise((resolve, reject)=>{
-    //         throw new Error("Error on purpose");
-    //     }).catch((reason)=>{
-    //         expect(reason.toString()).toEqual("Error: Error on purpose");
-    //     });
-    // });
-    //
-    // it("Promise is an object or function with a then method whose behavior conforms to this specification", ()=>{
-    //     let promise = new Promise((resolve, reject)=>{});
-    //     expect(typeof promise.then).toEqual("function");
-    // });
-    //
-    // it("Promise will call resolve function when fulfilled, and change status to resolved", (done)=>{
-    //     let promise = new Promise((resolve, reject)=>{
-    //         process.nextTick(()=>{
-    //             resolve();
-    //             expect(promise.status).toEqual(PromiseStatus.RESOLVED);
-    //             done();
-    //         });
-    //     });
-    //     expect(promise.status).toEqual(PromiseStatus.PENDING);
-    // });
-    //
-    // it("Promise will call reject function when failed, and change status to rejected", (done)=>{
-    //     let promise = new Promise((resolve, reject)=>{
-    //         process.nextTick(()=>{
-    //             reject();
-    //             expect(promise.status).toEqual(PromiseStatus.REJECTED);
-    //             done();
-    //         });
-    //     });
-    //     expect(promise.status).toEqual(PromiseStatus.PENDING);
-    // });
-    //
-    // it("Promise will not change status after complete(reject)", (done)=>{
-    //     let promise = new Promise((resolve, reject)=>{
-    //         process.nextTick(()=>{
-    //             reject();
-    //             resolve();
-    //             expect(promise.status).toEqual(PromiseStatus.REJECTED);
-    //             done();
-    //         });
-    //     });
-    // });
-    //
-    // it("Promise will not change status after complete(resolve)", (done)=>{
-    //     let promise = new Promise((resolve, reject)=>{
-    //         process.nextTick(()=>{
-    //             resolve();
-    //             reject();
-    //             expect(promise.status).toEqual(PromiseStatus.RESOLVED);
-    //             done();
-    //         });
-    //     });
-    // });
+    it("Promise is an object or function with a then method whose behavior conforms to this specification", ()=>{
+        let promise = new Promise((resolve, reject)=>{});
+        expect(typeof promise.then).toEqual("function");
+    });
+
+    it("Promise will call resolve function when fulfilled, and change status to resolved", (done)=>{
+        let promise = new Promise((resolve, reject)=>{
+            process.nextTick(()=>{
+                resolve();
+                expect(promise.status).toEqual(PromiseStatus.RESOLVED);
+                done();
+            });
+        });
+        expect(promise.status).toEqual(PromiseStatus.PENDING);
+    });
+
+    it("Promise will call reject function when failed, and change status to rejected", (done)=>{
+        let promise = new Promise((resolve, reject)=>{
+            process.nextTick(()=>{
+                reject();
+                expect(promise.status).toEqual(PromiseStatus.REJECTED);
+                done();
+            });
+        });
+        expect(promise.status).toEqual(PromiseStatus.PENDING);
+    });
+
+    it("Promise will not change status after complete(reject)", (done)=>{
+        let promise = new Promise((resolve, reject)=>{
+            process.nextTick(()=>{
+                reject();
+                resolve();
+                expect(promise.status).toEqual(PromiseStatus.REJECTED);
+                done();
+            });
+        });
+    });
+
+    it("Promise will not change status after complete(resolve)", (done)=>{
+        let promise = new Promise((resolve, reject)=>{
+            process.nextTick(()=>{
+                resolve();
+                reject();
+                expect(promise.status).toEqual(PromiseStatus.RESOLVED);
+                done();
+            });
+        });
+    });
 });
 
 describe("The Then Method", ()=>{
